80270f27d691e683de4d1028976ad8692d4b50da
깃Jserin <jsr3643@naver.com>
2023-03-08 12:25:39 +0900

    0308

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,46 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* System.out.println("ㅎㅇㅎㅇ\nㅎㅇㅎㅇ\nㅎㅇㅎㅇ\nㅎㅇㅎㅇ\nㅎㅇㅎㅇ\nㅎㅇㅎㅇ"); */
+        /* int a = 5;
+    int b = 10;
+    int c;
+
+    c = a;
+    a = b;
+    b = c;
+
+    System.out.println(a);
+    System.out.println(b); */
+        /* int x = 10;
+        System.out.println(x);
+        System.out.println("x");
+        System.out.println("x" + 10);
+        System.out.println(x +10);
+
+        System.out.println("x: " + (x + 2));
+        System.out.println("x: " + (x - 2));
+        System.out.println("x: " + (x * 2));
+        System.out.println("x: " + (x / 2));
+        System.out.println("x: " + (x % 2)); */
+
+        int age = 35;
+        System.out.println("당신의 나이 : " + age);
+
+        /* if (age >= 20) {
+            System.out.println("성인입니다.");
+        } else {
+            System.out.println(("미성년자입니다."));
+        } */
+
+        // 청년 검사
+        if (age >= 20 && age <= 34) {
+            System.out.println("청년입니다.");
+        } else {
+            System.out.println("청년이 아닙니다.");
+        }
+
+
+
     }
 }
