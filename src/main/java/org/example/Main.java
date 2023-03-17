0cc4f9f86d4287d8d826d6acd262c7e821fae0d9
Jserin <jsr3643@naver.com>
2023-03-17 10:32:21 +0900

    0317commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -461,7 +461,60 @@ public class Main {
         a전사.a무기 = new 칼();
          */
 
-        전사 a전사 = new 전사();
+        /* 전사 a전사 = new 전사();
+
+        String 이름 = "칸";
+        a전사.이름 = 이름;
+        a전사.나이 = 20;
+        a전사.자기소개();
+
+        a전사.나이++;
+        a전사.자기소개();
+
+        a전사.나이 = 30;
+        a전사.이름 = "카니";
+        a전사.자기소개();
+
+        a전사.a무기 = new 활();
+        a전사.공격();
+        // 출력 : 카니가 활로 공격합니다.
+
+        a전사.a무기 = new 칼();
+        a전사.공격();
+        // 출력 : 카니가 칼로 공격합니다. */
+
+        //0316
+        /* int i = 로봇.get정수();
+        boolean b = 로봇.get논리();
+        사람 a사람 = 로봇.get사람();
+        사람 a사람2 = 로봇.get사람2();
+        사람 a사람3 = 로봇.get사람3();
+        사람 a사람4 = 로봇.get사람4();
+        사람 a사람5 = 로봇.get사람5(123, false);
+        로봇.get사람6(123, false); */
+
+        /* 사람 a사람 = new 사람();
+        a사람.달리기_속력 = 20;
+        a사람.달리다();
+        // 사람이 20km/h로 달립니다.
+        a사람.달리다();
+        // 사람이 20km/h로 달립니다.
+
+        a사람.달리기_속력 = 50;
+        a사람.달리다();
+        // 사람이 50km/h로 달립니다.
+
+        a사람.나이 = 20;
+        a사람.a왼팔 = new 팔(); */
+
+        /* 전사 a전사 = new 전사();
+
+        a전사.나이 = 20;
+        a전사.a무기 = new 활();
+        a전사.a무기 = new 칼();
+         */
+
+        /* 전사 a전사 = new 전사();
 
         String 이름 = "칸";
         a전사.이름 = 이름;
@@ -483,12 +536,405 @@ public class Main {
         a전사.공격();
         // 출력 : 카니가 칼로 공격합니다.
 
+         */
+
+        /* 전사 a전사 = new 전사();
+        a전사.공격();
+        // 출력 => 아직 무기가 없습니다.
+        a전사.a무기 = new 칼();
+        a전사.공격();
+        // 출력 => 칼로 공격합니다.
+
+         */
+
+        /* 전사 a전사 = new 전사();
+
+        a전사.a무기1 = new 칼();
+        a전사.공격();
+        // 출력 => 전사가 칼로 공격합니다.
+
+        a전사.a무기1 = new 활();
+        a전사.공격();
+        // 출력 => 전사가 활로 공격합니다.
+
+        //a전사.a무기2 = new 칼();
+        //a전사.공격();
+        // 출력 => 전사가 활로 공격합니다.
+        // 출력 => 전사가 칼로 공격합니다. */
+
+        //생성자
+        /* 사람 a사람 = new 사람();
+        a사람.나이 = 10;
+        a사람.a왼팔 = new 팔();
+
+        System.out.println(a사람.a왼팔.길이 + "cm");
+
+        전사 a전사 = new 전사();
+        a전사.소개();
+
+         */
+
+
+        //0317
+        //0317 32강_1
+        /* 캐릭터 a플레이어_캐릭터;
+
+        a플레이어_캐릭터 = new 전사();
+        a플레이어_캐릭터.공격();
+        // 출력 : 전사가 대검으로 공격합니다.
+
+        a플레이어_캐릭터 = new 마법사();
+        a플레이어_캐릭터.공격();
+        // 출력 : 마법사가 파이어볼로 공격합니다.
+
+         */
+
+        //0317 32강_2
+        /* 전사 a전사0 = new 전사();
+        a전사0.나이 = 20;
+        a전사0.이름 = "칸";
+        a전사0.성격 = "차가움";
+        a전사0.a무기 = new 활();
+
+        전사 a전사1 = new 전사();
+        전사 a전사2 = new 전사();
+        전사 a전사3 = new 전사();
+        전사 a전사4 = new 전사();
+        전사 a전사5 = new 전사();
+
+        System.out.println(a전사0.이름);
+        // 출력 : 칸
+        System.out.println(a전사1.이름);
+        // 출력 : No Name
+        System.out.println(a전사2.이름);
+        // 출력 : No Name
+        System.out.println(a전사3.이름);
+        // 출력 : No Name
+        System.out.println(a전사4.이름);
+        // 출력 : No Name
+        System.out.println(a전사5.이름);
+        // 출력 : No Name
+
+         */
+
+        //0317 32강_3
+        /* 사람 a사람 = new 사람();
+        a사람.나이 = 10;
+        a사람.a왼팔 = new 팔();
+
+        System.out.println(a사람.a왼팔.길이 + "cm");
+        // 출력 : 100cm */
+
+        //0317 32강_4
+        /* new 사람().걷다();
+
+        System.out.println(new 사람().a왼팔.길이 + "cm");
+        // 출력 : 100cm */
+
+        //0317 32강_5 this 넣는 문제
+        /* int k = 10; // main 함수 안에서만 산다.
+
+        사람 a사람 = new 사람();
+        a사람.나이 = 20;
+
+        int dan = 8;  // main 함수 안에서만 산다.
+        for ( int i = 1; i < 9; i++ ) {
+            // i => for block 안에서만 산다.
+            System.out.println(dan + " * " + i + " = " + dan * i);
+        }
+
+        System.out.println(a사람.나이);
+        a사람.걷다();
+
+         */
+
+        //0317 32강_6,7,8
+        int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
+
+        System.out.println(엄지손가락_길이 + "cm");
+        // 출력 : 5cm
+
 
     }
 }
 
-//0315 전사 문제
+//0317 32강_7 6번에서 생성자 1개
+class 사람 {
+    팔 a왼팔;
+    사람 () {
+        this.a왼팔 = new 팔();
+    }
+
+}
+class 팔 {
+    손 a손 = new 손();
+}
+class 손 {
+    손가락 a엄지손가락 = new 손가락();
+}
+class 손가락 {
+    int 길이 = 5;
+}
+
+//0317 32강_7 6번에서 생성자 3개
+/* class 사람 {
+    팔 a왼팔;
+    사람 () {
+        this.a왼팔 = new 팔();
+    }
+
+}
+class 팔 {
+    손 a손;
+    팔 () {
+        this.a손 = new 손();
+    }
+}
+class 손 {
+    손가락 a엄지손가락;
+    손 () {
+        this.a엄지손가락 = new 손가락();
+    }
+}
+class 손가락 {
+    int 길이 = 5;
+}
+
+ */
+
+//0317 32강_6
+/* class 사람 {
+    팔 a왼팔;
+    사람 () {
+        this.a왼팔 = new 팔();
+    }
+
+}
+class 팔 {
+    손 a손;
+    팔 () {
+        this.a손 = new 손();
+    }
+}
+class 손 {
+    손가락 a엄지손가락;
+    손 () {
+        this.a엄지손가락 = new 손가락();
+    }
+}
+class 손가락 {
+    int 길이;
+    손가락 () {
+        this.길이 = 5;
+    }
+}
+
+ */
+
+//0317 32강_5
+/* class 사람 {
+    // 인스턴스 변수
+    int 나이;
+
+    void 걷다() {
+        int 나이 = 22;
+
+        System.out.println(this.나이 + "살인 홍길동씨가 걷습니다.");
+    }
+} */
+
+//0317 32강_4
+/* class 사람 {
+    팔 a왼팔;
+    사람 a사람;
+    void 걷다() {
+        System.out.println("사람 걷는다.");
+    }
+    사람() {
+        this.a왼팔 = new 팔();
+    }
+}
+class 팔 {
+    int 길이 = 100;
+} */
+
+//0317 32강_3
+/* class 사람 {
+    int 나이;
+    팔 a왼팔;
+}
+class 팔{
+    int 길이;
+    팔() {
+        this.길이 = 100;
+    }
+}*/
+
+//0317 32강_2
+/* class 전사 {
+    int 나이;
+    String 이름;
+    String 성격;
+    활 a무기;
+}
+class 활 {} */
+
+//0317 32강_1
+/* abstract class 캐릭터 {
+    abstract void 공격();
+}
+class 전사 extends 캐릭터 {
+    void 공격 () {
+        System.out.println("전사가 대검으로 공격합니다.");
+    }
+}
+class 마법사 extends 캐릭터 {
+    void 공격 () {
+        System.out.println("마법사가 파이어볼로 공격합니다.");
+    }
+}
+
+ */
+
+//0316
+// 생성자
+/* class 사람 {
+    int 나이;
+    팔 a왼팔;
+}
+class 팔 {
+    int 길이;
+    // 밑이 생성자함수 void 같은 거 붙이면 안됨.
+    팔 () {
+        this.길이 = 100;
+    }
+}
+
 class 전사 {
+    String 이름;
+    // 생성자 함수는 클래스 이름과 똑같아야 함.
+    전사(){
+        this.이름 = "홍길동";
+    }
+    void 소개() {
+        System.out.println("전사의 이름은 " + 이름 + "입니다.");
+    }
+}
+
+ */
+
+/* class 전사 {
+    무기 a무기1;
+    무기 a무기2;
+    void 공격 () {
+       a무기1.실행();
+    }
+}
+abstract class 무기 {
+    abstract void 실행();
+}
+class 칼 extends 무기{
+    void 실행() {
+        System.out.println("전사가 칼로 공격합니다.");
+    }
+}
+class 활 extends 무기{
+    void 실행() {
+        System.out.println("전사가 활로 공격합니다.");
+    }
+}*/
+
+
+
+/* class 전사 {
+    String 이름;
+    int 나이;
+    무기 a무기;
+    void 자기소개() {
+        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
+    }
+
+    void 공격() {
+        a무기.이름 = this.이름;
+        a무기.실행();
+    }
+}
+
+abstract class 무기 extends 전사 {
+    String 이름;
+    abstract void 실행();
+}
+
+class 활 extends 무기 {
+    void 실행() {
+        System.out.println(이름 + "가 활로 공격합니다.");
+    }
+}
+
+class 칼 extends 무기 {
+    void 실행() {
+        System.out.println(이름 + "가 칼로 공격합니다.");
+    }
+}
+
+ */
+
+/* class 사람 {
+    int 나이;
+    int 달리기_속력;
+    팔 a왼팔;
+    void 달리다() {
+        System.out.println("사람이 " + 달리기_속력 + "km/h로 달립니다.");
+    }
+}
+class 팔 {}
+ */
+
+/* class 로봇 {
+    static int get정수() {
+        return 1;
+    }
+
+    static boolean get논리() {
+        return true;
+    }
+
+    static 사람 get사람() {
+        사람 a사람 = new 사람();
+        return a사람;
+    }
+
+    static 사람 get사람2() {
+        사람 a사람2 = new 사람();
+        return a사람2;
+    }
+
+    static 사람 get사람3() {
+        사람 a사람3 = new 사람();
+        return a사람3;
+    }
+
+    static 사람 get사람4() {
+        사람 a사람4 = new 사람();
+        return a사람4;
+    }
+
+    static 사람 get사람5(int a, boolean b) {
+        사람 a사람5 = new 사람();
+        return a사람5;
+    }
+
+    static 사람 get사람6(int a, boolean b) {
+        사람 a사람6 = new 사람();
+        return a사람6;
+    }
+}
+
+class 사람 {}*/
+
+
+//0315 전사 문제
+/*class 전사 {
     String 이름;
     int 나이;
     무기 a무기;
@@ -514,6 +960,8 @@ class 활 extends 무기 {
         System.out.println( 사용자명 + "가 활로 공격합니다.");
     }
 }
+
+ */
 
 /*
 class car {
